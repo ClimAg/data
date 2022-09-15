84af8c6cc5bb30f04f820ef840e264da6fb4f63d
Nithiya Streethran <nmstreethran@gmail.com>
2022-09-15 14:53:23 +0100

    update eurocordex scripts

--- eurocordex/update-eurocordex-wget.sh
+++ eurocordex/update-eurocordex-wget.sh
@@ -9,16 +9,16 @@
 # ESGF portal is in the working directory
 
 for node in esgf.ceda.ac.uk esg-dn1.nsc.liu.se;
-do rm -f -- wget-$node.sh
-for filename in $(grep -l $node wget-*.sh);
+do rm -f -- wget-${node}.sh
+for filename in $(grep -l ${node} wget-*.sh);
 do awk '
 !/19511201/ && !/19560101/ && !/19610101/ && !/19660101/ && !/19700101/ &&
 !/19710101/ &&
 !/20060101/ && !/20110101/ && !/20160101/ && !/20210101/ && !/20260101/ &&
 !/20310101/ && !/20310101/ && !/20360101/ && !/20710101/ && !/20760101/ &&
 !/20810101/ && !/20860101/ && !/20910101/ && !/20960101/
-' $filename > wget-$node.sh &&
-sed -i 's/egrep/grep -E/g' wget-$node.sh &&
+' ${filename} > wget-${node}.sh &&
+sed -i 's/egrep/grep -E/g' wget-${node}.sh &&
 awk -v timestamp="$(date)" '
 NR==5{ print "\
 # NOTE: this file has been modified to remove data files that do not\
@@ -26,7 +26,7 @@ NR==5{ print "\
 \n# Additionally, egrep has been replaced with grep -E.\
 \n# Last updated by N. M. Streethran - " timestamp ".\
 \n#" }1
-' wget-$node.sh > temp.sh
-mv temp.sh wget-$node.sh;
+' wget-${node}.sh > temp.sh
+mv temp.sh wget-${node}.sh;
 done;
 done
