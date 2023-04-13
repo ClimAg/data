a46b8d9a7e08e3e6f6056833ae7a5b05f32401e4
Nithiya Streethran <nmstreethran@gmail.com>
2023-04-13 17:59:59 +0100

    update MERA functions

--- MERA/mera_modvege_input.py
+++ MERA/mera_modvege_input.py
@@ -9,95 +9,104 @@ import os
 import sys
 import pandas as pd
 import xarray as xr
+from data.MERA.mera_data_et import mera_calculate_et
 
-# directory of processed MÉRA netCDF files
-DATA_DIR = os.path.join("/run/media/nms/MyPassport", "MERA", "netcdf_day")
 
-# list of netCDF variable files
-var_list = [
-    "11_105_2_0",  # 2 m temperature
-    "61_105_0_4",  # total precipitation
-    "117_105_0_4",  # global irradiance
-    "PET",  # evapotranspiration
-]
+def mera_modvege_input(years, spinup=False):
 
-# dictionary to store Xarray datasets
-ds = {}
+    mera_calculate_et(years)
 
-for var in var_list:
-    ds[var] = xr.open_mfdataset(
-        glob.glob(os.path.join(DATA_DIR, f"MERA_{var}_day.nc")),
-        chunks="auto",
-        decode_coords="all",
+    # directory of processed MÉRA netCDF files
+    DATA_DIR = os.path.join("/run/media/nms/MyPassport", "MERA", "netcdf_day")
+
+    # list of netCDF variable files
+    var_list = [
+        "11_105_2_0",  # 2 m temperature
+        "61_105_0_4",  # total precipitation
+        "117_105_0_4",  # global irradiance
+        "PET",  # evapotranspiration
+    ]
+
+    # dictionary to store Xarray datasets
+    ds = {}
+
+    for var in var_list:
+        ds[var] = xr.open_mfdataset(
+            glob.glob(os.path.join(DATA_DIR, f"MERA_{years[0]}_{years[1]}_{var}_day.nc")),
+            chunks="auto",
+            decode_coords="all",
+        )
+
+    # obtain CRS info
+    data_crs = ds["11_105_2_0"].rio.crs
+
+    # drop the height dimension from the datasets
+    for v in var_list[:-1]:
+        ds[v] = ds[v].isel(height=0)
+
+    # ## Calculate photosynthetically active radiation (PAR)
+
+    # Papaioannou et al. (1993) - irradiance ratio
+    ds["117_105_0_4"] = ds["117_105_0_4"].assign(
+        PAR=ds["117_105_0_4"]["grad"] * 0.473
+    )
+    ds["117_105_0_4"]["PAR"].attrs[
+        "long_name"
+    ] = "Surface Photosynthetically Active Radiation"
+    ds["117_105_0_4"]["PAR"].attrs["units"] = "MJ m⁻² day⁻¹"
+
+    # ## Merge datasets
+
+    # merge datasets
+    ds = xr.combine_by_coords(
+        [ds["11_105_2_0"], ds["61_105_0_4"], ds["117_105_0_4"], ds["PET"]],
+        combine_attrs="drop_conflicts",
+        compat="override",
     )
 
-# obtain CRS info
-data_crs = ds["11_105_2_0"].rio.crs
+    # drop global radiation
+    ds = ds.drop_vars(["grad"])
 
-# drop the height dimension from the datasets
-for v in var_list[:-1]:
-    ds[v] = ds[v].isel(height=0)
+    # rename other variables
+    ds = ds.rename({"t": "T", "tp": "PP"})
 
-# ## Calculate photosynthetically active radiation (PAR)
+    # assign dataset name
+    ds.attrs["dataset"] = "IE_MERA_FC3hr_3_day"
 
-# Papaioannou et al. (1993) - irradiance ratio
-ds["117_105_0_4"] = ds["117_105_0_4"].assign(
-    PAR=ds["117_105_0_4"]["grad"] * 0.473
-)
-ds["117_105_0_4"]["PAR"].attrs[
-    "long_name"
-] = "Surface Photosynthetically Active Radiation"
-ds["117_105_0_4"]["PAR"].attrs["units"] = "MJ m⁻² day⁻¹"
+    # reassign CRS
+    ds.rio.write_crs(data_crs, inplace=True)
 
-# ## Merge datasets
+    if spinup:
+        # ## Extend data to spin-up year
 
-# merge datasets
-ds = xr.combine_by_coords(
-    [ds["11_105_2_0"], ds["61_105_0_4"], ds["117_105_0_4"], ds["PET"]],
-    combine_attrs="drop_conflicts",
-    compat="override",
-)
+        # copy first year data
+        ds_interp = ds.interp(
+            time=pd.date_range(f"{years[0] - 1}-01-01", f"{years[0] - 1}-12-31", freq="D"),
+            kwargs={"fill_value": None},
+        )
 
-# drop global radiation
-ds = ds.drop_vars(["grad"])
+        ds_interp.rio.write_crs(data_crs, inplace=True)
 
-# rename other variables
-ds = ds.rename({"t": "T", "tp": "PP"})
+        # merge spin-up year with first two years of the main data
+        ds_interp = xr.combine_by_coords(
+            [ds_interp, ds.sel(time=slice(str(years[0]), str(years[0] + 1)))]
+        )
 
-# assign dataset name
-ds.attrs["dataset"] = "IE_MERA_FC3hr_3_day"
+        # shift first year of the main data to the spin-up year
+        ds_interp = ds_interp.shift(time=-ds_interp.sel(time=str(years[0])).dims["time"])
 
-# reassign CRS
-ds.rio.write_crs(data_crs, inplace=True)
+        # keep only spin-up year
+        ds_interp = ds_interp.sel(time=str(years[0]))
 
-# ## Extend data to spin-up year
+        # merge with main dataset
+        ds = xr.combine_by_coords([ds, ds_interp])
 
-# copy 1981 data to 1980
-ds_interp = ds.interp(
-    time=pd.date_range("1980-01-01", "1980-12-31", freq="D"),
-    kwargs={"fill_value": None},
-)
+    ds.rio.write_crs(data_crs, inplace=True)
 
-ds_interp.rio.write_crs(data_crs, inplace=True)
+    # ## Save data
 
-# merge spin-up year with first two years of the main data
-ds_interp = xr.combine_by_coords(
-    [ds_interp, ds.sel(time=slice("1981", "1982"))]
-)
+    ds.to_netcdf(os.path.join("data", "MERA", f"{ds.attrs['dataset']}_{years[0]}_{years[1]}.nc"))
 
-# shift first year of the main data to the spin-up year
-ds_interp = ds_interp.shift(time=-ds_interp.sel(time="1980").dims["time"])
+    sys.exit()
 
-# keep only spin-up year
-ds_interp = ds_interp.sel(time="1980")
-
-# merge with main dataset
-ds = xr.combine_by_coords([ds, ds_interp])
-
-ds.rio.write_crs(data_crs, inplace=True)
-
-# ## Save data
-
-ds.to_netcdf(os.path.join("data", "MERA", ds.attrs["dataset"] + ".nc"))
-
-sys.exit()
+mera_modvege_input(years=(2012, 2019))
